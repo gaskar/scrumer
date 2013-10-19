c73b14899a7ead05d47b1fc6353c49c569ede683
gaskar <kagasparyan@gmail.com>
2013-10-19 21:10:50 +0400

    new version, socket.io fixing

--- web.js
+++ web.js
@@ -4,7 +4,15 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var io = require('socket.io').listen(8800);
+var http = require('http');
+var server = http.createServer(app);
+
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+server.listen(process.env.PORT || 5000);
+
+var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
@@ -12,7 +20,3 @@ io.sockets.on('connection', function (socket) {
         console.log(data);
     });
 });
-
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
