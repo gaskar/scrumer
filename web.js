f818a508198ac346dd39c560b733d7fc1af3c70a
gaskar <kagasparyan@gmail.com>
2013-10-19 20:46:11 +0400

    uncomment server starting code

--- web.js
+++ web.js
@@ -13,6 +13,6 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
-//app.use(express.logger('dev'));
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-//app.listen(process.env.PORT || 5000);
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
