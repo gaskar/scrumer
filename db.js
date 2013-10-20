4a775963e5c79d428ba5ebc368c7ef3ef8906a84
gaskar <kagasparyan@gmail.com>
2013-10-20 22:18:15 +0400

    db change

--- db.js
+++ db.js
@@ -2,6 +2,6 @@
  * Created by gaskar on 10/19/13.
  */
 module.exports = {
-    host: "scumertest:scrumertest@ds051368.mongolab.com:51368",
+    host: "heroku_app18770611:scrumertest1@ds051368.mongolab.com:51368",
     dbName: "heroku_app18770611"
 };
