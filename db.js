a49edbebff6fb49e02ae0adb8fe8a4472a95989b
gaskar <kagasparyan@gmail.com>
2013-10-20 21:40:46 +0400

    removing unnecessary files

--- db.js
+++ db.js
@@ -2,6 +2,6 @@
  * Created by gaskar on 10/19/13.
  */
 module.exports = {
-    host: "scumertest:scrumertest@ds051368.mongolab.com:51368",
-    dbName: "heroku_app18770611"
+    host: "localhost",
+    dbName: "scrumer"
 };
